9ca0d91c554ab5cf962f4c07dd2662cc79843aeb
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-10-17 13:38:39 -0600

    chore: create main files and folders

---
+++ js/nav.js
@@ -0,0 +1,12 @@
+// Navbar
+document.write(`
+  <nav>
+    <ul class="menu">
+      <li><a href="index.html" class="menu-link">Home</a></li>
+      <li><a href="portfolio.html" class="menu-link">Portfolio</a></li>
+      <li><a href="about.html" class="menu-link">About</a></li>
+      <li><a href="resume.html" class="menu-link">Resume</a></li>
+      <li><a href="contact.html" class="menu-link">Contact</a></li>
+    </ul>
+  </nav>
+`);
