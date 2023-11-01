eba9d4f6b527809fa00a4dc4f351785b86cd2223
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-11-01 04:50:17 -0600

    refactor and build: tags to main.css file | sections in aboutme.html

--- js/nav.js
+++ js/nav.js
@@ -5,7 +5,7 @@ document.write(`
     <ul class="menu">
       <li><a href="index.html" class="menu-link">Home</a></li>
       <li><a href="portfolio.html" class="menu-link">Portfolio</a></li>
-      <li><a href="about.html" class="menu-link">About</a></li>
+      <li><a href="aboutme.html" class="menu-link">About</a></li>
       <li><a href="resume.html" class="menu-link">Resume</a></li>
       <li><a href="contact.html" class="menu-link">Contact</a></li>
     </ul>
